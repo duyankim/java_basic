9ea7f866dde419ff74bc25ba6c152f060dd312da
Duyan-Kim <arbolkim@gmail.com>
2021-03-14 22:48:49 +0900

    inheritance 실습

---
+++ src/objectTutorial/Wizard.java
@@ -0,0 +1,13 @@
+package objectTutorial;
+
+public class Wizard extends Hero {
+
+	public Wizard(String name) {
+		super(name);
+	}
+	
+	public void freezing() {
+		System.out.println("얼리기!");
+	}
+
+}
