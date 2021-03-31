ae9d37791073a1bddf88fb3406027b7f1e1a5853
duyankim <arbolkim@gmail.com>
2021-03-31 14:32:14 +0900

    환전 계산기 만들기

---
+++ src/computing/ConstValueClass.java
@@ -0,0 +1,11 @@
+package computing;
+
+public class ConstValueClass {
+	public static final double USD_RATE = 1134.30;
+	public static final double EUR_RATE = 1333.50;
+	public static final double JPY_RATE = 10.30;
+	public static final int CHANGE_TYPE_USD = 1;
+	public static final int CHANGE_TYPE_EUR = 2;
+	public static final int CHANGE_TYPE_YAN = 3;
+
+}
