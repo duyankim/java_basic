707eec7189e090c7db98abccca16b1041945183d
duyankim <arbolkim@gmail.com>
2021-05-09 22:49:17 +0900

    linkedList method 구현하기

--- src/dataStructure/LinkedList/MySingleLinkedList.java
+++ src/dataStructure/LinkedList/MySingleLinkedList.java
@@ -28,32 +28,102 @@ public class MySingleLinkedList<T> {
 		if (head == null) {
 			return null;
 		} 
-		
 		T temp = head.data;
 		head = head.next;
 		return temp;
 	}
 	
-	public void add(int index, T item) { // insert
+	public T removeAfter(Node<T> before) {
+		if (before.next == null) {
+			return null;
+		}
 		
-	}
-	
-	public void remove(int index) { // delete
-		
+		T temp = before.next.data;
+		before.next = before.next.next;
+		return temp;
 	}
 	
 	public int indexOf(T item) { // search
+		Node<T> p = head;
+		int index = 0;
+		while(p != null) {
+			if (p.data.equals(item)) {
+				return index;
+			}
+			p = p.next;
+			index++;
+		}
 		return -1;
 	}
 	
-	public T get(int index) {
-		return null;
+	public Node<T> getNode(int index) {
+		if (index < 0 || index >= size) {
+			return null;
+		}
+		Node<T> p = head;
+		for (int i = 0; i < index; i++) {
+			p = p.next;
+		}
+		return p;
 	}
 	
+	public T get(int index) {
+		if (index < 0 || index >= size) {
+			return null;
+		}
+		return getNode(index).data;
+	}
+	
+	public void add(int index, T item) { // insert
+		if (index < 0 || index > size) {
+			return;
+		}
+		if (index == 0) {
+			addFirst(item);
+		} else {
+			Node<T> q = getNode(index - 1);
+			addAfter(q, item);
+		}
+	}
+	
+	public T remove(int index) { // delete
+		if (index < 0 || index >= size) {
+			return null;
+		}
+		if (index == 0) {
+			return removeFirst();
+		}
+		Node<T> prev = getNode(index - 1);
+		return removeAfter(prev);
+	}
+	
+	public T remove (T item) {
+		Node<T> p = head, q = null;
+		while (p != null && !p.data.equals(item)) {
+			q = p;
+			p = p.next;
+		}
+		if (p == null) {
+			return null;
+		}
+		if (q == null) {
+			return removeFirst();
+		} else {
+			return removeAfter(q);
+		}
+	}
+
 	public static void main(String[] args) {
 		MySingleLinkedList<String> list = new MySingleLinkedList<String>();
 		
-
+		list.add(0, "Saturday");
+		list.add(1, "Friday");
+		list.add(0, "Monday");
+		list.add(2, "Tuesday");
+		
+		String str = list.get(2);
+		list.remove(2);
+		int pos = list.indexOf("Friday");
 	}
 
 }
